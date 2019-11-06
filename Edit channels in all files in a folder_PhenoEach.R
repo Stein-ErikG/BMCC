9299fcee966f4b09fbe4b5df53c1b460bf4052a2
Gullaksen <30684606+Gullaksen@users.noreply.github.com>
2019-11-06 10:09:45 +0100

    Update Edit channels in all files in a folder_PhenoEach.R

--- Edit channels in all files in a folder_PhenoEach.R
+++ Edit channels in all files in a folder_PhenoEach.R
@@ -1,59 +1,26 @@
 library("flowCore")
 
-## Fix the PhenoEach K30 channels in all files in a folder (data_dir)
+## Fix the name of a channel in one ore more .fcs files in a folder (data_dir).
+## Need this if you have used Phenograph in Matlab/SightOf on more than one file
 
 data_dir = '/Users/mmasg/Dropbox/Rstudio/Parker_ICI/Caroline data/renamed/'
 setwd('/Users/mmasg/Dropbox/Rstudio/Parker_ICI/Caroline data/renamed/')
-files  <- list.files(pattern = "_edited_2.fcs")
-files
+files  <- list.files(pattern = ".fcs") # Use pattern if you need to select certain .fcs files
+files # View your selection
 
-library(premessa)
-concatenate_fcs_files(files, output.file = '/Users/mmasg/Dropbox/Rstudio/Parker_ICI/Caroline data/renamed/pool.fcs')
-
-ff <- read.flowSet(files[1])
-pData(ff[[1]]@parameters)
+ff <- read.flowSet(files[1]) # Read the first .fcs files
+pData(ff[[1]]@parameters) # Have a look at you channels to find the channel number you want to change
+                          # In the example below we need to change the 66th channel to "PhenoGraph Each K30"
 
 for (file in files){
   ff <- read.FCS(file)
-  file_name <- file
+  file_name <- file # Store the filename so that it can be edited for saving later
   ff@parameters@data$name[66] = "PhenoGraph Each K30"
   ff@parameters@data$desc[66] = "PhenoGraph Each K30"
   colnames(ff@exprs)[66] = "PhenoGraph Each K30"
   ff@description$`$P66N` = "PhenoGraph Each K30"
   ff@description$`$P66S` = "PhenoGraph Each K30"
-  ff@exprs[,1:62] = 5*(sinh(ff@exprs[,1:62]))
-  new_file_name <- gsub(".fcs", "_transrelabelled.fcs", file_name)
-  write.FCS(ff, filename = paste0(data_dir, new_file_name), what="numeric", delimiter = "\\")
+  #ff@exprs[,1:62] = 5*(sinh(ff@exprs[,1:62])) # This reverses the arcsinh transformation. Dont use if you dont need
+  new_file_name <- gsub(".fcs", "_transrelabelled.fcs", file_name) # Makes new filename. You can edit "_transrelabelled.fcs" 
+  write.FCS(ff, filename = paste0(data_dir, new_file_name), what="numeric", delimiter = "\\") # Save the edited file. 
 }
-
-for (file in files){
-  ff <- read.FCS(file)
-  file_name <- file
-  ff@parameters@data$name[36] = "PhenoGraph Each K30"
-  ff@parameters@data$desc[36] = "PhenoGraph Each K30"
-  colnames(ff@exprs)[36] = "PhenoGraph Each K30"
-  ff@description$`$P36N` = "PhenoGraph Each K30"
-  ff@description$`$P36S` = "PhenoGraph Each K30"
-  
-  ff@parameters@data$name[38] = "PhenoGraph Each K15"
-  ff@parameters@data$desc[38] = "PhenoGraph Each K15"
-  colnames(ff@exprs)[38] = "PhenoGraph Each K15"
-  ff@description$`$P38N` = "PhenoGraph Each K15"
-  ff@description$`$P38S` = "PhenoGraph Each K15"
-  
-  new_file_name <- gsub(".fcs", "_edited_2.fcs", file_name)
-  write.FCS(ff, filename = paste0(data_dir, new_file_name), what="numeric", delimiter = "\\")
-} 
-
-new_files  <- list.files(pattern = "_edited.fcs")
-NNVdataset <- read.flowSet(new_files)
-pData(NNVdataset[[1]]@parameters)
-
-
-x=5
-
-
-y =(asinh((x)/5))
-
-
-
